596a3a7cbb135e5e6aebafe5b001ec0d3ed647a0
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-04-08 18:01:52 -0500

    Array traversal methods 2

---
+++ arrays3.js
@@ -0,0 +1,28 @@
+const products = [
+  { name: "Bicycle", price: 200 },
+  { name: "TV", price: 400 },
+  { name: "Book", price: 30 },
+  { name: "Cellphone", price: 500 },
+  { name: "Laptop", price: 600 },
+  { name: "Keyboard", price: 200 },
+  { name: "Earphones", price: 400 },
+];
+
+// const foundProduct = products.find(function (product) {
+//   return product.name === "Laptop";
+// });
+
+// const foundProduct = products.find((product) => product.price === 400);
+
+// console.log(foundProduct);
+
+// products.forEach((product) => console.log(product.name));
+
+// const checkProduct = products.some((product) => product.price <= 200);
+// console.log(checkProduct);
+
+// products.splice(0, 1, { name: "Mouse", price: 40 });
+// console.log(products);
+
+// products.splice(4, 0, { name: "Mouse", price: 50 });
+// console.log(products);
